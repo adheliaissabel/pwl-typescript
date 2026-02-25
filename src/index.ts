86a664fc72ac7f9163575d96fb344e9ed1a7a4a8
Dhelya <h1101241001@student.untan.ac.id>
2026-02-25 04:46:11 +0000

    update

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ function hello(nama: string, umur: number): string {
     return `Selamat datang ${nama} di PWL, umur saya = ${umur}`;
 }
 
-const mahasiswa = "Andy";
+const mahasiswa = "Adhel";
 const umur: number = 19;
 
 console.log(hello(mahasiswa, umur));
